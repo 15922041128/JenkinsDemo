7057fbca3c191dd3aac6a91ea0d3a55552a502ec
kamehameha <konamikce@qq.com>
2019-06-19 11:24:47 +0800

    add test methods

--- src/main/java/com/wode/jenkinsDemo/test/Job2.java
+++ src/main/java/com/wode/jenkinsDemo/test/Job2.java
@@ -4,7 +4,6 @@ package com.wode.jenkinsDemo.test;
  * Created by Administrator on 2019/6/18.
  */
 public class Job2 {
-
     public static void main(String[] args) {
         System.out.println("run job2");
     }
---
+++ src/test/java/com/wode/jenkinsDemo/test/Job2Test.java
@@ -0,0 +1,15 @@
+package com.wode.jenkinsDemo.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/6/19.
+ */
+public class Job2Test {
+
+    @Test
+    public void testJob1() {
+        System.out.println("run testjob2");
+
+    }
+}
