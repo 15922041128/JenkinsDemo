d2fa1391ea77714ebb6d4febce1df81805c122da
kamehameha <konamikce@qq.com>
2019-06-19 11:28:27 +0800

    add testjob3

---
+++ src/test/java/com/wode/jenkinsDemo/test/Job3Test.java
@@ -0,0 +1,15 @@
+package com.wode.jenkinsDemo.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/6/19.
+ */
+public class Job3Test {
+
+    @Test
+    public void testJob3() {
+        System.out.println("run testjob3");
+
+    }
+}
